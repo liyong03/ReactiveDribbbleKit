a9cbd829a6006606cd555071285a250d910086b3
liyong03 <liyong03@gmail.com>
2014-06-17 21:06:34 +0800

    init the demo project, and using pod to import necessory libs

---
+++ ReactiveDribbbleKitDemo/ReactiveDribbbleKitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReactiveDribbbleKitDemo
+//
+//  Created by Yong Li on 6/17/14.
+//  Copyright (c) 2014 Yong Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
