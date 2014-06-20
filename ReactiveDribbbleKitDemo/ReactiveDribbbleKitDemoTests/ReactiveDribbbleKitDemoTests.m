b8cf0ffab4f7af93505f76778a1717e577c2e756
liyong03 <liyong03@gmail.com>
2014-06-20 18:02:59 +0800

    add test case for getplayer

--- ReactiveDribbbleKitDemo/ReactiveDribbbleKitDemoTests/ReactiveDribbbleKitDemoTests.m
+++ ReactiveDribbbleKitDemo/ReactiveDribbbleKitDemoTests/ReactiveDribbbleKitDemoTests.m
@@ -7,17 +7,23 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+#import "YLDribbbleEngine.h"
+#import "YLReactiveDribbbleEngine.h"
 
 @interface ReactiveDribbbleKitDemoTests : XCTestCase
 
 @end
 
-@implementation ReactiveDribbbleKitDemoTests
+@implementation ReactiveDribbbleKitDemoTests {
+    OCMockObject* _mockDribbbleEngine;
+}
 
 - (void)setUp
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    _mockDribbbleEngine = [OCMockObject mockForClass:[YLDribbbleEngine class]];
 }
 
 - (void)tearDown
@@ -26,9 +32,26 @@
     [super tearDown];
 }
 
-- (void)testExample
+
+- (void)testGetProfileName
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    void (^theBlock)(NSInvocation *) = ^(NSInvocation *invocation) {
+        void (^passedBlock)( YLDribbbleUser* user );
+        [invocation getArgument:&passedBlock atIndex:3];
+        //NSString* str = [NSString stringWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"getPlayer" withExtension:@"json"] encoding:NSUTF8StringEncoding error:nil];
+        NSURL* url = [[NSBundle bundleForClass:self.class] URLForResource:@"getPlayer" withExtension:@"json"];
+        NSData* data = [NSData dataWithContentsOfURL:url];
+        id obj = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+        YLDribbbleUser* player = [MTLJSONAdapter modelOfClass:YLDribbbleUser.class fromJSONDictionary:obj error:nil];
+        passedBlock(player);
+    };
+    [[[_mockDribbbleEngine stub] andDo:theBlock] getPlayerWithUsername:[OCMArg any] successBlock:[OCMArg any] failedBlock:[OCMArg any]];
+    __block YLDribbbleUser* user = nil;
+    [[YLReactiveDribbbleEngine getPlayerWithUsername:@""] subscribeNext:^(id x) {
+        user = x;
+    }];
+    
+    XCTAssert(user.userID == 1);
 }
 
 @end
