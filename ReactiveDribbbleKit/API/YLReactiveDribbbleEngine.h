44030e81d514187a35f80a1037d74ce78fe05b70
liyong03 <liyong03@gmail.com>
2014-06-21 23:27:45 +0800

    finish other api

--- ReactiveDribbbleKit/API/YLReactiveDribbbleEngine.h
+++ ReactiveDribbbleKit/API/YLReactiveDribbbleEngine.h
@@ -11,6 +11,30 @@
 
 @interface YLReactiveDribbbleEngine : NSObject
 
+// player
 + (RACSignal*)getPlayerWithUsername:(NSString*)username;
+
+
+// player list
++ (RACSignal*)getPlayerFollowingWithUsername:(NSString*)username page:(NSInteger)page;
++ (RACSignal*)getPlayerFollowerWithUsername:(NSString*)username page:(NSInteger)page;
++ (RACSignal*)getPlayerDrafteeWithUsername:(NSString*)username page:(NSInteger)page;
+
+// shot
++ (RACSignal*)getShotWithID:(NSUInteger)shotID;
+
+// shot list
++ (RACSignal*)getPlayerShotsWithUsername:(NSString*)username page:(NSInteger)page;
++ (RACSignal*)getFollowingShotsWithUsername:(NSString*)username page:(NSInteger)page;
+
++ (RACSignal*)getLikeShotsWithUsername:(NSString*)username page:(NSInteger)page;
+
++ (RACSignal*)getEveryoneShotsWithPage:(NSInteger)page;
++ (RACSignal*)getPopularShotsWithPage:(NSInteger)page;
++ (RACSignal*)getDebutsShotsWithPage:(NSInteger)page;
+
+// comment list
++ (RACSignal*)getCommentsOfShot:(NSUInteger)shotID withPage:(NSInteger)page;
+
 
 @end
--- ReactiveDribbbleKit/API/YLReactiveDribbbleEngine.m
+++ ReactiveDribbbleKit/API/YLReactiveDribbbleEngine.m
@@ -12,7 +12,7 @@
 @implementation YLReactiveDribbbleEngine
 
 + (RACSignal*)getPlayerWithUsername:(NSString*)username {
-    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+    return [[[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
         AFHTTPRequestOperation* operation = [YLDribbbleEngine getPlayerWithUsername:username
                                                                        successBlock:^(YLDribbbleUser *user) {
                                                                            [subscriber sendNext:user];
@@ -23,7 +23,182 @@
         return [RACDisposable disposableWithBlock:^{
             [operation cancel];
         }];
-    }];
+    }] publish] autoconnect];
+}
+
++ (RACSignal*)getPlayerFollowingWithUsername:(NSString*)username page:(NSInteger)page {
+    return [[[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        AFHTTPRequestOperation* operation = [YLDribbbleEngine getPlayerFollowingWithUsername:username page:page
+                                                                                successBlock:^(YLDribbbleUserList *list) {
+                                                                                    [subscriber sendNext:list];
+                                                                                    [subscriber sendCompleted];
+                                                                                } failedBlock:^(NSError *error) {
+                                                                                    [subscriber sendError:error];
+                                                                                }];
+        return [RACDisposable disposableWithBlock:^{
+            [operation cancel];
+        }];
+    }] publish] autoconnect];
+}
++ (RACSignal*)getPlayerFollowerWithUsername:(NSString*)username page:(NSInteger)page {
+    return [[[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        AFHTTPRequestOperation* operation = [YLDribbbleEngine getPlayerFollowerWithUsername:username page:page
+                                                                               successBlock:^(YLDribbbleUserList *list) {
+                                                                                   [subscriber sendNext:list];
+                                                                                   [subscriber sendCompleted];
+                                                                               } failedBlock:^(NSError *error) {
+                                                                                   [subscriber sendError:error];
+                                                                               }];
+        return [RACDisposable disposableWithBlock:^{
+            [operation cancel];
+        }];
+    }] publish] autoconnect];
+}
++ (RACSignal*)getPlayerDrafteeWithUsername:(NSString*)username page:(NSInteger)page {
+    return [[[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        AFHTTPRequestOperation* operation = [YLDribbbleEngine getPlayerDrafteeWithUsername:username page:page
+                                                                              successBlock:^(YLDribbbleUserList *list) {
+                                                                                  [subscriber sendNext:list];
+                                                                                  [subscriber sendCompleted];
+                                                                              } failedBlock:^(NSError *error) {
+                                                                                  [subscriber sendError:error];
+                                                                              }];
+        return [RACDisposable disposableWithBlock:^{
+            [operation cancel];
+        }];
+    }] publish] autoconnect];
+}
+
+
+// shot
++ (RACSignal*)getShotWithID:(NSUInteger)shotID {
+    return [[[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        AFHTTPRequestOperation* operation = [YLDribbbleEngine getShotWithID:shotID
+                                                               successBlock:^(YLDribbbleShot *shot) {
+                                                                   [subscriber sendNext:shot];
+                                                                   [subscriber sendCompleted];
+                                                               } failedBlock:^(NSError *error) {
+                                                                   [subscriber sendError:error];
+                                                               }];
+        return [RACDisposable disposableWithBlock:^{
+            [operation cancel];
+        }];
+    }] publish] autoconnect];
+}
+
+// shot list
++ (RACSignal*)getPlayerShotsWithUsername:(NSString*)username page:(NSInteger)page {
+    return [[[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        AFHTTPRequestOperation* operation = [YLDribbbleEngine getPlayerShotsWithUsername:username page:page
+                                                                            successBlock:^(YLDribbbleShotList *list) {
+                                                                                [subscriber sendNext:list];
+                                                                                [subscriber sendCompleted];
+                                                                            } failedBlock:^(NSError *error) {
+                                                                                [subscriber sendError:error];
+                                                                            }];
+        return [RACDisposable disposableWithBlock:^{
+            [operation cancel];
+        }];
+    }] publish] autoconnect];
+}
++ (RACSignal*)getFollowingShotsWithUsername:(NSString*)username page:(NSInteger)page {
+    
+    return [[[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        AFHTTPRequestOperation* operation = [YLDribbbleEngine getFollowingShotsWithUsername:username page:page
+                                                                               successBlock:^(YLDribbbleShotList *list) {
+                                                                                   [subscriber sendNext:list];
+                                                                                   [subscriber sendCompleted];
+                                                                               } failedBlock:^(NSError *error) {
+                                                                                   [subscriber sendError:error];
+                                                                               }];
+        return [RACDisposable disposableWithBlock:^{
+            [operation cancel];
+        }];
+    }] publish] autoconnect];
+    
+}
+
++ (RACSignal*)getLikeShotsWithUsername:(NSString*)username page:(NSInteger)page {
+    
+    return [[[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        AFHTTPRequestOperation* operation = [YLDribbbleEngine getLikeShotsWithUsername:username page:page
+                                                                          successBlock:^(YLDribbbleShotList *list) {
+                                                                              [subscriber sendNext:list];
+                                                                              [subscriber sendCompleted];
+                                                                          } failedBlock:^(NSError *error) {
+                                                                              [subscriber sendError:error];
+                                                                          }];
+        return [RACDisposable disposableWithBlock:^{
+            [operation cancel];
+        }];
+    }] publish] autoconnect];
+    
+}
+
++ (RACSignal*)getEveryoneShotsWithPage:(NSInteger)page {
+    
+    return [[[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        AFHTTPRequestOperation* operation = [YLDribbbleEngine getEveryoneShotsWithPage:page
+                                                                          successBlock:^(YLDribbbleShotList *list) {
+                                                                              [subscriber sendNext:list];
+                                                                              [subscriber sendCompleted];
+                                                                          } failedBlock:^(NSError *error) {
+                                                                              [subscriber sendError:error];
+                                                                          }];
+        return [RACDisposable disposableWithBlock:^{
+            [operation cancel];
+        }];
+    }] publish] autoconnect];
+    
+}
++ (RACSignal*)getPopularShotsWithPage:(NSInteger)page {
+    
+    return [[[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        AFHTTPRequestOperation* operation = [YLDribbbleEngine getPopularShotsWithPage:page
+                                                                         successBlock:^(YLDribbbleShotList *list) {
+                                                                             [subscriber sendNext:list];
+                                                                             [subscriber sendCompleted];
+                                                                         } failedBlock:^(NSError *error) {
+                                                                             [subscriber sendError:error];
+                                                                         }];
+        return [RACDisposable disposableWithBlock:^{
+            [operation cancel];
+        }];
+    }] publish] autoconnect];
+    
+}
++ (RACSignal*)getDebutsShotsWithPage:(NSInteger)page {
+    
+    return [[[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        AFHTTPRequestOperation* operation = [YLDribbbleEngine getDebutsShotsWithPage:page
+                                                                        successBlock:^(YLDribbbleShotList *list) {
+                                                                            [subscriber sendNext:list];
+                                                                            [subscriber sendCompleted];
+                                                                        } failedBlock:^(NSError *error) {
+                                                                            [subscriber sendError:error];
+                                                                        }];
+        return [RACDisposable disposableWithBlock:^{
+            [operation cancel];
+        }];
+    }] publish] autoconnect];
+    
+}
+
+// comment list
++ (RACSignal*)getCommentsOfShot:(NSUInteger)shotID withPage:(NSInteger)page {
+    
+    return [[[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        AFHTTPRequestOperation* operation = [YLDribbbleEngine getCommentsOfShot:shotID withPage:page
+                                                                   successBlock:^(YLDribbbleCommentList *list) {
+                                                                       [subscriber sendNext:list];
+                                                                       [subscriber sendCompleted];
+                                                                   } failedBlock:^(NSError *error) {
+                                                                       [subscriber sendError:error];
+                                                                   }];
+        return [RACDisposable disposableWithBlock:^{
+            [operation cancel];
+        }];
+    }] publish] autoconnect];
 }
 
 @end
